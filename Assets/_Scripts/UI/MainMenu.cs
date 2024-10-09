c37ce671f5c56c2ac362ca3832ee6df8f918255d
Stephen Ho <stephen.ho.232@gmail.com>
2024-10-10 02:16:26 +0700

    Add menu button + credit scene pt1

--- Assets/_Scripts/UI/MainMenu.cs
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject m_creditPanel;
     public void LoadToGameplayScene()
     {
         SceneManager.LoadSceneAsync("GameplayScene",LoadSceneMode.Single);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (m_creditPanel.activeSelf)
+            {
+                m_creditPanel.SetActive(false);
+            }
+        }
     }
 }
