e8d08b18073bb3727c4bfa82e5aa63c83abb1172
Stephen Ho <stephen.ho.232@gmail.com>
2024-10-13 14:10:27 +0700

    Add explode particle

--- Assets/_Scripts/Player/PlayerBullet.cs
+++ Assets/_Scripts/Player/PlayerBullet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using SGGames.Scripts.Managers;
 using UnityEngine;
 
@@ -9,11 +10,16 @@ public class PlayerBullet : MonoBehaviour
     [SerializeField] private LayerMask m_obstacleMask;
     [SerializeField] private DamageHandler m_damageHandler;
     
+    [SerializeField] private Animator m_animator;
+
+    private int m_explodeAnimParam = Animator.StringToHash("Explode");
     private Vector2 m_direction;
     private bool m_isAlive;
 
     private void OnEnable()
     {
+        m_spriteRenderer.enabled = true;
+        m_animator.Play("Idle");
         m_damageHandler.OnHit += OnHitTarget;
     }
 
@@ -24,6 +30,15 @@ public class PlayerBullet : MonoBehaviour
 
     private void OnHitTarget(GameObject target)
     {
+        StartCoroutine(OnDestroying());
+    }
+
+    private IEnumerator OnDestroying()
+    {
+        m_isAlive = false;
+        m_spriteRenderer.enabled = false;
+        m_animator.SetTrigger(m_explodeAnimParam);
+        yield return new WaitForSeconds(0.35f);
         SelfDestroy();
     }
 
@@ -36,14 +51,43 @@ public class PlayerBullet : MonoBehaviour
         {
             m_spriteRenderer.transform.rotation = Quaternion.AngleAxis(90,Vector3.forward);
         }
+
+        RotateExplodeParticle();
         m_isAlive = true;
+    }
+
+    private void RotateExplodeParticle()
+    {
+        if (m_direction == Vector2.left)
+        {
+            m_animator.gameObject.transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+            return;
+        }
+        
+        if (m_direction == Vector2.right)
+        {
+            m_animator.gameObject.transform.rotation = Quaternion.AngleAxis(-90, Vector3.forward);
+            return;
+        }
+        
+        if (m_direction == Vector2.up)
+        {
+            m_animator.gameObject.transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            return;
+        }
+        
+        if (m_direction == Vector2.down)
+        {
+            m_animator.gameObject.transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            return;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (LayerManager.IsInLayerMask(other.gameObject.layer, m_obstacleMask))
         {
-            SelfDestroy();
+            StartCoroutine(OnDestroying());
         }
     }
     
